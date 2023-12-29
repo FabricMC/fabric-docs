d6f93e5f7506986c6725a7b598944bc5a03df48e
modmuss50 <modmuss50@gmail.com>
2023-12-29 21:33:47 +0000

    Add Reference mod

---
+++ reference/1.20.4/src/main/java/com/example/docs/FabricDocsReference.java
@@ -0,0 +1,22 @@
+package com.example.docs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricDocsReference implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("fabric-docs-reference");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
