7e16ad0f2f97cc1179ecef80c1ad23a5b4af1136
modmuss <modmuss50@gmail.com>
2023-12-29 21:49:50 +0000

    Add Reference mod (#7)
    * Add Reference mod
    
    * 1.20.4 -> latest

---
+++ reference/latest/src/main/java/com/example/docs/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.example.docs.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.MinecraftServer;
+
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
