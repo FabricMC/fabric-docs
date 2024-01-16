5ee55d0b5068267d740ae7fdcc9491bc4384a52b
Superkat32 <superkat1806@gmail.com>
2024-01-15 19:29:30 -0500

    Registering Particles Page
    FEAT: Added a new page found within the develop/particles folder. This page guides users through the process of registering a new particle.

--- reference/latest/src/client/java/com/example/docs/FabricDocsReferenceClient.java
+++ reference/latest/src/client/java/com/example/docs/FabricDocsReferenceClient.java
@@ -1,10 +1,15 @@
 package com.example.docs;
 
+import net.minecraft.client.particle.EndRodParticle;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 
 public class FabricDocsReferenceClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		ParticleFactoryRegistry.getInstance().register(FabricDocsReference.MY_PARTICLE, EndRodParticle.Factory::new);
 	}
 }
--- reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
+++ reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
@@ -1,15 +1,24 @@
 package com.example.docs;
 
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 
 public class FabricDocsReference implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("fabric-docs-reference");
+	public static final String MOD_ID = "fabric-docs-reference";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	//This DefaultParticleType gets called when you want to use your particle in code
+	public static final DefaultParticleType MY_PARTICLE = FabricParticleTypes.simple();
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +27,7 @@ public class FabricDocsReference implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier(MOD_ID, "my_particle"), MY_PARTICLE);
 	}
 }
