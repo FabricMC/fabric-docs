f120863f926d6928e3b36740f2ebb82211a3b5be
Superkat32 <superkat1806@gmail.com>
2024-01-15 20:10:53 -0500

    Code Style fix
    FIX: Hopefully fixed the incorrect imports

--- reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
+++ reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
@@ -1,11 +1,12 @@
 package com.example.docs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
