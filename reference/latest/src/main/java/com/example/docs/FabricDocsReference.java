0047a732c10deab157af4557f9ed98ea6be69e5c
Superkat32 <89557012+Superkat32@users.noreply.github.com>
2024-01-21 14:24:35 -0500

    "Creating Custom Particles" Page (#20)
    * Registering Particles Page
    
    FEAT: Added a new page found within the develop/particles folder. This page guides users through the process of registering a new particle.
    
    * Code Style fix
    
    FIX: Hopefully fixed the incorrect imports
    
    * Migrate to code reference functionality.
    
    * Add extra frontmatter information.
    
    * Disable comment indentation on spotless, conflicts with markdown plugin.
    
    * Revert "Disable comment indentation on spotless, conflicts with markdown plugin."
    
    This reverts commit 62d142205d50f6cb0714052c04167f88bca87a80.
    
    * Solve checkstyle issues.
    
    * Discard changes to package-lock.json
    
    ---------

--- reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
+++ reference/latest/src/main/java/com/example/docs/FabricDocsReference.java
@@ -3,13 +3,26 @@ package com.example.docs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 
 public class FabricDocsReference implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("fabric-docs-reference");
+	public static final String MOD_ID = "fabric-docs-reference";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	//#particle_register_main
+	// This DefaultParticleType gets called when you want to use your particle in code
+	public static final DefaultParticleType MY_PARTICLE = FabricParticleTypes.simple();
+
+	//#particle_register_main
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +31,10 @@ public class FabricDocsReference implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		//#particle_register_main
+		// Register our custom particle type in the mod initializer.
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier(MOD_ID, "my_particle"), MY_PARTICLE);
+		//#particle_register_main
 	}
 }
