4dec9641c63a5f493c38960c7fa14d7d9fa9d0f9
Calum <contact@mineblock11.dev>
2024-01-23 15:03:16 +0000

    Checkstyle fixes.

--- reference/latest/src/client/java/com/example/docs/rendering/RenderingConceptsEntrypoint.java
+++ reference/latest/src/client/java/com/example/docs/rendering/RenderingConceptsEntrypoint.java
@@ -1,26 +1,19 @@
 package com.example.docs.rendering;
 
-
 import com.mojang.blaze3d.systems.RenderSystem;
+import org.joml.Matrix4f;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.AxisTransformation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
-import org.joml.Matrix4f;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL11;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class RenderingConceptsEntrypoint implements ClientModInitializer {
 	public float totalTickDelta = 0F;
@@ -55,7 +48,7 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 			// We don't need to scale the Z axis since it's on the HUD and 2D.
 			matrices.scale(scaleAmount, scaleAmount, 1F);
 			// :::2
-			matrices.scale(1 / scaleAmount , 1 / scaleAmount, 1F);
+			matrices.scale(1 / scaleAmount, 1 / scaleAmount, 1F);
 			matrices.translate(60f, 60f, 0f);
 			// :::3
 			// Lerp between 0 and 360 degrees over time.
@@ -66,7 +59,6 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 			// :::3
 
 			// :::1
-
 			// Initialize the buffer using the specified format and draw mode.
 			buffer.begin(VertexFormat.DrawMode.TRIANGLE_STRIP, VertexFormats.POSITION_COLOR);
 
@@ -82,7 +74,6 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 
 			// Draw the buffer onto the screen.
 			tessellator.draw();
-
 			// :::1
 			// :::2
 
@@ -92,7 +83,7 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 			matrices.pop();
 			// :::2
 			// :::1
-        });
+		});
 		// :::1
 	}
 }
