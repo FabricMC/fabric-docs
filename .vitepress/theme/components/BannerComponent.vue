bc180639c16743c695e943d00d59a485812d19d4
Calum <contact@mineblock11.dev>
2024-02-11 19:27:12 +0000

    WIP warning.

---
+++ .vitepress/theme/components/BannerComponent.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { useElementSize } from '@vueuse/core';
+import { onBeforeMount, onMounted, ref, watchEffect } from 'vue';
+
+const el = ref<HTMLElement>();
+const { height } = useElementSize(el);
+
+watchEffect(() => {
+  if (height.value) {
+    document.documentElement.style.setProperty(
+      '--vp-layout-top-height',
+      `${height.value + 16}px`
+    );
+  }
+});
+
+const dismiss = () => {
+  localStorage.setItem(
+    'dismissBanner',
+    (Date.now() + 8.64e7 * 1).toString() // current time + 1 day
+  );
+  document.documentElement.classList.add('banner-dismissed');
+};
+
+onBeforeMount(() => {
+  if (localStorage.getItem('dismissBanner') !== null) {
+    var date = parseInt(localStorage.getItem('dismissBanner')!);
+    if (Date.now() > date) {
+      dismiss();
+    }
+  }
+});
+</script>
+
+<template>
+  <div ref="el" class="banner">
+    <div class="text">
+      Fabric Documentation is a work in progress. If you find any issues, please report them on the <a
+        href="https://github.com/fabricmc/fabric-docs/issues" target="_blank" rel="noopener noreferrer">GitHub
+        repository</a>.
+    </div>
+
+    <button type="button" @click="dismiss">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <path
+          d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<style>
+.banner-dismissed {
+  --vp-layout-top-height: 0px !important;
+}
+
+html {
+  --vp-layout-top-height: 88px;
+}
+
+@media (min-width: 375px) {
+  html {
+    --vp-layout-top-height: 64px;
+  }
+}
+
+@media (min-width: 768px) {
+  html {
+    --vp-layout-top-height: 40px;
+  }
+}
+</style>
+
+<style scoped>
+.banner-dismissed .banner {
+  transform: translateY(-100%);
+}
+
+.banner {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: var(--vp-z-index-layout-top);
+
+  font-family: monospace;
+
+  padding: 8px;
+  text-align: center;
+
+  transition: transform 0.3s ease;
+
+  background: rgb(207, 114, 21);
+  color: #fff;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+.text {
+  flex: 1;
+}
+
+a {
+  text-decoration: underline;
+}
+
+svg {
+  width: 20px;
+  height: 20px;
+  margin-left: 8px;
+}
+</style>
