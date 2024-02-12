170735ab5df965849f45c05a1d84102cc2ddb39f
Calum H <contact@mineblock11.dev>
2024-02-12 14:59:24 +0000

    Warning Banner (#38)
    * Develop Sidebar + Setup spanish for translators
    
    * Undo copying of spanish files.
    
    * Update Crowdin configuration file
    
    * Delete crowdin.yml file
    
    * Fix versioning configuration
    
    * Publishing Workflow
    
    * WIP warning.
    
    * Remove VitePress GitHub Pages workflow
    
    * Fix banner logic.
    
    * Fix flash
    
    * Refactor BannerComponent.vue

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 
 import PageAuthorComponent from './components/PageAuthorComponent.vue'
+import BannerComponent from './components/BannerComponent.vue'
 
 // Import style fixes and customizations.
 import './style.css'
@@ -10,7 +11,8 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'aside-outline-after': () => h(PageAuthorComponent)
+      'aside-outline-after': () => h(PageAuthorComponent),
+      'layout-top': () => h(BannerComponent)
     })
   }
 };
