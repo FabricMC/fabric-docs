b3520428f71bbec24fb34cb36a04417bbfe05cbd
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-12-24 14:17:39 -0800

    Simple Advancements
    Also run datagen

--- develop/data-generation/advancements.md
+++ develop/data-generation/advancements.md
@@ -21,9 +21,25 @@ To finish setup, add this provider to your `DataGeneratorEntrypoint`.
 
 @[code lang=java transcludeWith=:::datagen-advancements:2](@/reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceAdvancementGenerator.java)
 
-## Creating Advancements {#creating-advancements}
-
 ## Advancement Structure {#advancement-structure}
+
+An advancement is made up a few different components. Along with the requirements, called "criterion," it may have:
+
+- An `AdvancementDisplay` that tells the game how to show the advancement to players,
+- `AdvancementRequirements`, which are different from criterion (we'll go over these later), %% TODO: What are these? %%
+- `AdvancementRewards`, which the player recives for completing the advancement.
+- A `CriterionMerger`, which tells the advancement how to handle multiple criterion, and
+- A parent `Advancement`, which organizes the heirarchy you see on the "Advancements" screen.
+
+## Simple Advancements {#simple-advancements}
+
+Here's a simple advancement for getting a dirt block:
+
+@[code lang=java transcludeWith=:::datagen-advancements:3](@/reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceAdvancementProvider.java)
+
+::: details JSON Output
+@[code lang=json](@/reference/latest/src/main/generated/data/minecraft/advancement/fabric-docs-reference/get_dirt.json)
+:::
 
 ## Custom Criterion {#custom-criterion}
 
--- reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceAdvancementProvider.java
+++ reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceAdvancementProvider.java
@@ -1,10 +1,19 @@
 package com.example.docs.datagen;
 
+import com.example.docs.FabricDocsReference;
+
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 
+import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
+import net.minecraft.advancement.AdvancementFrame;
+import net.minecraft.advancement.AdvancementRewards;
+import net.minecraft.advancement.criterion.InventoryChangedCriterion;
+import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -16,7 +25,25 @@ public class FabricDocsReferenceAdvancementProvider extends FabricAdvancementPro
 
 	@Override
 	public void generateAdvancement(RegistryWrapper.WrapperLookup wrapperLookup, Consumer<AdvancementEntry> consumer) {
-
+		// :::datagen-advancements:1
+		// :::datagen-advancements:3
+		AdvancementEntry getDirt = Advancement.Builder.create()
+				.display(
+						Items.DIRT, // The display icon
+						Text.literal("Your First Dirt Block"), // The title
+						Text.literal("Now make a house from it"), // The description
+						Identifier.ofVanilla("textures/gui/advancements/backgrounds/adventure.png"), // Background image for the tab in the advancements page, if this is a root advancement (has no parent)
+						AdvancementFrame.TASK, // TASK, CHALLENGE, or GOAL
+						true, // Show the toast when completing it
+						true, // Announce it to chat
+						false // Hide it in the advancement tab until it's achieved
+				)
+				// "got_dirt" is the name referenced by other advancements when they want to have "requirements."
+				.criterion("got_dirt", InventoryChangedCriterion.Conditions.items(Items.DIRT))
+				// Give the advancement an id
+				.build(consumer, FabricDocsReference.MOD_ID + "/get_dirt");
+		// :::datagen-advancements:3
+		// :::datagen-advancements:1
 	}
 }
 // :::datagen-advancements:1
