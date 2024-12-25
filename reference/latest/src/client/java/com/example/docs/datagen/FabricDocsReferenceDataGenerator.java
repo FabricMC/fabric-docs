4b5de16a20b4a443c0fd798f97543ef3a07085a8
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-12-24 20:03:28 -0800

    Cleanup - setup

--- reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceDataGenerator.java
+++ reference/latest/src/client/java/com/example/docs/datagen/FabricDocsReferenceDataGenerator.java
@@ -1,16 +1,16 @@
 package com.example.docs.datagen;
-// :::datagen-setup:2
+// :::datagen-setup:generator
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class FabricDocsReferenceDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		// :::datagen-setup:2
-		// :::datagen-setup:3
+		// :::datagen-setup:generator
+		// :::datagen-setup:pack
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		// :::datagen-setup:3
-		// :::datagen-setup:2
+		// :::datagen-setup:pack
+		// :::datagen-setup:generator
 	}
 }
-// :::datagen-setup:2
+// :::datagen-setup:generator
