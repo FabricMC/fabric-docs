5955ae47b86be9dda7bd4fe32cb59445eb6e55b3
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-12-26 21:16:02 -0800

    Add loot tables to sidebar

--- .vitepress/sidebars/develop.ts
+++ .vitepress/sidebars/develop.ts
@@ -210,6 +210,10 @@ export default [
 		{
 			text: "devlop.dataGeneration.recipes",
 			link: "/develop/data-generation/recipes"
+		},
+		{
+			text: "devlop.dataGeneration.lootTables",
+			link: "/develop/data-generation/loot-tables"
 		}
 	  ]
   },
