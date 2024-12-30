48711927bca074341cd20f3bb63a2f59a486d724
natri0 <dev@lina.moe>
2024-12-30 15:02:14 +0200

    Block Entities article (#238)

--- .vitepress/sidebars/develop.ts
+++ .vitepress/sidebars/develop.ts
@@ -103,6 +103,10 @@ export default [
         text: "develop.blocks.blockstates",
         link: "/develop/blocks/blockstates",
       },
+      {
+        text: "develop.blocks.block-entities",
+        link: "/develop/blocks/block-entities",
+      }
     ],
   },
   {
