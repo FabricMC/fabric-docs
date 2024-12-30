5a265db278e3af0d15544c6ac4a041bfb8fe6bd3
modmuss50 <modmuss50@gmail.com>
2024-12-30 17:26:16 +0000

    Remove @Environment usage.

--- reference/latest/src/client/java/com/example/docs/rendering/blockentity/CounterBlockEntityRenderer.java
+++ reference/latest/src/client/java/com/example/docs/rendering/blockentity/CounterBlockEntityRenderer.java
@@ -7,13 +7,9 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.RotationAxis;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
 import com.example.docs.block.entity.custom.CounterBlockEntity;
 
 // :::1
-@Environment(EnvType.CLIENT)
 public class CounterBlockEntityRenderer implements BlockEntityRenderer<CounterBlockEntity> {
 	// :::1
 
