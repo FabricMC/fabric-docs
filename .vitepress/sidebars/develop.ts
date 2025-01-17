2140ab1bfde8458bd0f040e57f6f858c77fd7242
dicedpixels <dicedpixels@hotmail.com>
2025-01-17 22:28:15 +0530

    add sidebar entries

--- .vitepress/sidebars/develop.ts
+++ .vitepress/sidebars/develop.ts
@@ -248,6 +248,10 @@ export default [
         link: "/develop/events",
       },
       {
+        text: "develop.misc.networking",
+        link: "/develop/networking",
+      },
+      {
         text: "develop.misc.text-and-translations",
         link: "/develop/text-and-translations",
       },
