cc756672a302275107447ad9c26cf6e15e12710c
Fellteros <aloisprochazka6@gmail.com>
2025-01-17 18:55:04 +0100

    Created example ModelProvider

---
+++ reference/1.21/src/client/java/com/example/docs/datagen/FabricDocsModelProvider.java
@@ -0,0 +1,23 @@
+package com.example.docs.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+
+public class FabricDocsModelProvider extends FabricModelProvider {
+	public FabricDocsModelProvider(FabricDataOutput output) {
+		super(output);
+	}
+
+	@Override
+	public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+	}
+
+	@Override
+	public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+
+	}
+}
