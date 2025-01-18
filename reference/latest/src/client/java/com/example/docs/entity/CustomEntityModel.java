297e2ec4ad3431ed40dfc98de2216e182442cdbb
JaaiDead <arcticquests.dev@gmail.com>
2025-01-18 22:24:13 +0530

    Implement custom entity in server-side.

---
+++ reference/latest/src/client/java/com/example/docs/entity/CustomEntityModel.java
@@ -0,0 +1,6 @@
+package com.example.docs.entity;
+
+public class CustomEntityModel {
+        //Dummy CLass Generate stuff form block bench (recommended) :D //TODO: Temp Need Help & Review
+
+}
--- reference/latest/src/main/java/com/example/docs/entity/FabricDocsReferenceEntity.java
+++ reference/latest/src/main/java/com/example/docs/entity/FabricDocsReferenceEntity.java
@@ -1,5 +1,19 @@
 package com.example.docs.entity;
 
-public class FabricDocsReferenceEntity {
-    
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ModInitializer;
+import com.example.docs.entity.FabricDocsReferanceCustomEntity;
+
+
+public class FabricDocsReferenceEntity implements ModInitializer {
+ 
+	public static final String MOD_ID = "fabric-docs-reference";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		FabricDocsReferanceCustomEntity.registerCustomEntity();
+	}
 }
