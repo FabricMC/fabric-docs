8ab48a43d320173d096f93cf25cf4b6bded8bdf3
Miroma <136986257+its-miroma@users.noreply.github.com>
2025-02-12 11:33:46 +0100

    fix build, filter for public org

--- .vitepress/data/maintainers.data.ts
+++ .vitepress/data/maintainers.data.ts
@@ -6,16 +6,29 @@ const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 export default {
   async load() {
-    const members = await octokit.paginate(
-      octokit.rest.teams.listMembersInOrg,
-      {
-        org: "FabricMC",
-        team_slug: "documentation",
+    const members = await (async () => {
+      try {
+        return await octokit.paginate(octokit.rest.teams.listMembersInOrg, {
+          org: "FabricMC",
+          team_slug: "documentation",
+        });
+      } catch (e) {
+        // Allows build without a GITHUB_TOKEN
+        return [];
       }
-    );
+    })();
 
     return members
       .filter((contributor) => contributor.login !== "FabricMCBot")
+      .filter(async (contributor) =>
+        (
+          await octokit.paginate(octokit.rest.orgs.listForUser, {
+            username: contributor.login,
+          })
+        )
+          .map((org) => org.login)
+          .includes("FabricMC")
+      )
       .map(
         (member) =>
           ({
