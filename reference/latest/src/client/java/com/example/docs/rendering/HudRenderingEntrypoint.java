b5b48ce8a4b205a4c77be18089dfa908cb594206
joe <burtonjae@hotmail.co.uk>
2025-02-27 20:04:16 +0000

    Make HudRenderingEntrypoint less obtrusive

--- reference/latest/src/client/java/com/example/docs/rendering/HudRenderingEntrypoint.java
+++ reference/latest/src/client/java/com/example/docs/rendering/HudRenderingEntrypoint.java
@@ -27,16 +27,17 @@ public class HudRenderingEntrypoint implements ClientModInitializer {
 		int color = 0xFFFF0000; // Red
 		int targetColor = 0xFF00FF00; // Green
 
-		// You can use the Util.getMeasuringTimeMs(); function to get the current time in seconds.
-		double currentTime = Util.getMeasuringTimeMs();
+		// You can use the Util.getMeasuringTimeMs() function to get the current time in milliseconds.
+		// Divide by 1000 to get seconds.
+		double currentTime = Util.getMeasuringTimeMs() / 1000.0;
 
 		// "lerp" simply means "linear interpolation", which is a fancy way of saying "blend".
-		float lerpedAmount = MathHelper.abs(MathHelper.sin((float) (currentTime / 50.0f)));
+		float lerpedAmount = MathHelper.abs(MathHelper.sin((float) currentTime));
 		int lerpedColor = ColorHelper.lerp(lerpedAmount, color, targetColor);
 
 		// Draw a square with the lerped color.
 		// x1, x2, y1, y2, z, color
-		context.fill(0, 0, 100, 100, 0, lerpedColor);
+		context.fill(0, 0, 10, 10, 0, lerpedColor);
 	}
 }
 // :::1
