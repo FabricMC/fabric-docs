bb3ace2074bc33840d4da4cabc095119acca0a1b
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2025-09-23 15:43:01 -0400

    Update rendering concepts page (#362)
    * Update rendering concepts page
    
    * Uncomment note in hud.md
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Miroma <136986257+its-miroma@users.noreply.github.com>
    (cherry picked from commit 054180d5bb9e8b4a343adef6c6306bbbb9627b8f)

--- reference/latest/src/client/java/com/example/docs/rendering/RenderingConceptsEntrypoint.java
+++ reference/latest/src/client/java/com/example/docs/rendering/RenderingConceptsEntrypoint.java
@@ -1,6 +1,9 @@
 package com.example.docs.rendering;
 
+import org.joml.Matrix3x2fStack;
+
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
@@ -21,21 +24,19 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 
 	private HudElement hudLayer() {
 		return (drawContext, tickCounter) -> {
-			// TODO: fix advanced hud rendering example
-			/*
 			// :::1
 			if (false) {
 				return;
 			}
 
 			// :::2
-			MatrixStack matrices = drawContext.getMatrices();
+			Matrix3x2fStack matrices = drawContext.getMatrices();
 
 			// Store the total tick delta in a field, so we can use it later.
 			totalTickProgress += tickCounter.getTickProgress(true);
 
 			// Push a new matrix onto the stack.
-			matrices.push();
+			matrices.pushMatrix();
 			// :::2
 
 			// :::2
@@ -44,36 +45,29 @@ public class RenderingConceptsEntrypoint implements ClientModInitializer {
 
 			// Apply the scaling amount to the matrix.
 			// We don't need to scale the Z axis since it's on the HUD and 2D.
-			matrices.scale(scaleAmount, scaleAmount, 1F);
+			matrices.scale(scaleAmount, scaleAmount);
 			// :::2
-			matrices.scale(1 / scaleAmount, 1 / scaleAmount, 1F);
-			matrices.translate(60f, 60f, 0f);
+			matrices.scale(1 / scaleAmount, 1 / scaleAmount);
+			matrices.translate(60f, 60f);
 			// :::3
 			// Lerp between 0 and 360 degrees over time.
-			float rotationAmount = (float) (totalTickProgress / 50F % 360);
-			matrices.multiply(RotationAxis.POSITIVE_Z.rotation(rotationAmount));
-			// Shift entire diamond so that it rotates in its center.
-			matrices.translate(-20f, -40f, 0f);
+			float rotationAmount = totalTickProgress / 50F % 360;
+			matrices.rotate(rotationAmount);
+			// Shift entire square so that it rotates in its center.
+			matrices.translate(-20f, -40f);
 			// :::3
 
 			// :::1
-			drawContext.draw(vertexConsumerProvider -> {
-				VertexConsumer buffer = vertexConsumerProvider.getBuffer(RenderLayer.getGui());
-				buffer.vertex(20, 20, 5).color(0xFF414141);
-				buffer.vertex(5, 40, 5).color(0xFF000000);
-				buffer.vertex(35, 40, 5).color(0xFF000000);
-				buffer.vertex(20, 60, 5).color(0xFF414141);
-			});
+			drawContext.fillGradient(5, 20, 35, 60, 0xFF414141, 0xFF000000);
 			// :::1
 			// :::2
 
-			// ... write to the buffer.
+			// We do not need to manually write to the buffer. DrawContext methods write to GUI buffer in `GuiRenderer` at the end of preparation.
 
 			// Pop our matrix from the stack.
-			matrices.pop();
+			matrices.popMatrix();
 			// :::2
 			// :::1
-			 */
 		};
 	}
 }
