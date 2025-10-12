a73e85e756bcc1d88404e92e9ca3413fb6cac246
Miroma <136986257+its-miroma@users.noreply.github.com>
2025-10-12 21:36:27 +0200

    mv: ExampleModEntities -> ExampleModBlockEntities

--- reference/latest/src/main/java/com/example/docs/block/entity/ExampleModBlockEntities.java
+++ reference/latest/src/main/java/com/example/docs/block/entity/ExampleModBlockEntities.java
@@ -2,7 +2,7 @@ package com.example.docs.block.entity;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleModEntities implements ModInitializer {
+public class ExampleModBlockEntities implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModBlockEntities.initialize();
