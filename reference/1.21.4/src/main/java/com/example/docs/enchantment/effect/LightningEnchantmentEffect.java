c5fba39b07c61ac6697ab7599082390abc87b0e5
Miroma <136986257+its-miroma@users.noreply.github.com>
2025-10-15 23:12:39 +0200

    Bring back 1.21.4

---
+++ reference/1.21.4/src/main/java/com/example/docs/enchantment/effect/LightningEnchantmentEffect.java
@@ -0,0 +1,44 @@
+package com.example.docs.enchantment.effect;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.enchantment.EnchantmentEffectContext;
+import net.minecraft.enchantment.EnchantmentLevelBasedValue;
+import net.minecraft.enchantment.effect.EnchantmentEntityEffect;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+//#entrypoint
+public record LightningEnchantmentEffect(EnchantmentLevelBasedValue amount) implements EnchantmentEntityEffect {
+	public static final MapCodec<LightningEnchantmentEffect> CODEC = RecordCodecBuilder.mapCodec(instance ->
+			instance.group(
+					EnchantmentLevelBasedValue.CODEC.fieldOf("amount").forGetter(LightningEnchantmentEffect::amount)
+			).apply(instance, LightningEnchantmentEffect::new)
+	);
+
+	@Override
+	public void apply(ServerWorld world, int level, EnchantmentEffectContext context, Entity target, Vec3d pos) {
+		if (target instanceof LivingEntity victim) {
+			if (context.owner() != null && context.owner() instanceof PlayerEntity player) {
+				float numStrikes = this.amount.getValue(level);
+
+				for (float i = 0; i < numStrikes; i++) {
+					BlockPos position = victim.getBlockPos();
+					EntityType.LIGHTNING_BOLT.spawn(world, position, SpawnReason.TRIGGERED);
+				}
+			}
+		}
+	}
+
+	@Override
+	public MapCodec<? extends EnchantmentEntityEffect> getCodec() {
+		return CODEC;
+	}
+}
