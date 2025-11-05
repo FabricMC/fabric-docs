c01ad7a6734b0f2ad490dfdc0a8bfdcf6fc736fd
Cassian Godsted <cassian@godsted.com>
2025-11-05 15:00:36 -0500

    Remap gametests

--- reference/latest/src/test/java/com/example/docs/codec/BeanTypeTest.java
+++ reference/latest/src/test/java/com/example/docs/codec/BeanTypeTest.java
@@ -4,14 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.mojang.serialization.JsonOps;
+
+import net.minecraft.server.Bootstrap;
+
+import net.minecraft.world.item.ItemStack;
+
+import net.minecraft.world.item.Items;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import net.minecraft.Bootstrap;
 import net.minecraft.SharedConstants;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 // :::automatic-testing:4
 public class BeanTypeTest {
@@ -21,8 +25,8 @@ public class BeanTypeTest {
 	static void beforeAll() {
 		// :::automatic-testing:4
 		// :::automatic-testing:7
-		SharedConstants.createGameVersion();
-		Bootstrap.initialize();
+		SharedConstants.tryDetectVersion();
+		Bootstrap.bootStrap();
 		// :::automatic-testing:7
 		// :::automatic-testing:4
 		BeanTypes.register();
@@ -43,7 +47,7 @@ public class BeanTypeTest {
 		// I know this isn't related to beans, but I need an example :)
 		ItemStack diamondStack = new ItemStack(Items.DIAMOND, 65);
 
-		Assertions.assertTrue(diamondStack.isOf(Items.DIAMOND));
+		Assertions.assertTrue(diamondStack.is(Items.DIAMOND));
 		Assertions.assertEquals(65, diamondStack.getCount());
 	}
 }
