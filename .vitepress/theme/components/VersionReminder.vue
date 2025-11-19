4ea60555c9ce1851132176a726738104d554247d
Cassian <cassian@godsted.com>
2025-11-19 12:47:47 -0500

    Don't specify mappings when they're official

--- .vitepress/theme/components/VersionReminder.vue
+++ .vitepress/theme/components/VersionReminder.vue
@@ -20,8 +20,14 @@ const latest = computed(() => {
 });
 
 const old = computed(() => {
-  const split = options.value.oldVersionYarn.split("%s");
-  return [split[0], version.value, split.slice(1).join("%s")];
+  var versionSplit = version.value.split(".");
+  if (versionSplit[1] <= 21 && versionSplit[2] <= 10) {
+    const split = options.value.oldVersionYarn.split("%s");
+    return [split[0], version.value, split.slice(1).join("%s")];
+  } else {
+    const split = options.value.oldVersionMojang.split("%s");
+    return [split[0], version.value, split.slice(1).join("%s")];
+  }
 });
 </script>
 
