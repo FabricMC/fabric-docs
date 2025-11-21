89fa5a3c95673efb5dfbe3fe953b1775806db807
Cassian <cassian@godsted.com>
2025-11-21 12:10:41 -0500

    checkstyle

--- reference/latest/src/main/java/com/example/docs/entity/attribute/ModAttributes.java
+++ reference/latest/src/main/java/com/example/docs/entity/attribute/ModAttributes.java
@@ -4,10 +4,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 
 import com.example.docs.ExampleMod;
