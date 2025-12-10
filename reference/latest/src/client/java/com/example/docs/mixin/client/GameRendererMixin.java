a34f473f4acb9de7b92551bd1acd0f77fa7e8954
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-12-10 13:36:27 -0500

    Revert "Remove rendering information"
    This reverts commit 8ce9a3d59782610a4cb5bcafada45cb0d49cce56.

--- reference/latest/src/client/java/com/example/docs/mixin/client/GameRendererMixin.java
+++ reference/latest/src/client/java/com/example/docs/mixin/client/GameRendererMixin.java
@@ -7,14 +7,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.renderer.GameRenderer;
 
-//import com.example.docs.rendering.CustomRenderPipeline;
+import com.example.docs.rendering.CustomRenderPipeline;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 	@Inject(method = "close", at = @At("RETURN"))
 	private void onGameRendererClose(CallbackInfo ci) {
-		/* FIXME 1.21.11
 		CustomRenderPipeline.getInstance().close();
-		 */
 	}
 }
