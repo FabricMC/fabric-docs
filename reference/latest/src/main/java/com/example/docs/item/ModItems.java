634d015bfed024b80681c64e1286e1d79f6989da
Cassian <cassian@godsted.com>
2025-12-14 19:46:53 -0500

    checkstyle

--- reference/latest/src/main/java/com/example/docs/item/ModItems.java
+++ reference/latest/src/main/java/com/example/docs/item/ModItems.java
@@ -14,7 +14,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.AxeItem;
