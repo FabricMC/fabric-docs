7b955cc19241b794e49a14c4d6e20ec1219bee2b
Miroma <its.miroma@proton.me>
2026-01-01 20:39:13 +0100

    redirect item-group -> creative-tab

--- .vitepress/redirects.ts
+++ .vitepress/redirects.ts
@@ -2,4 +2,13 @@
 // when `from` is a RegExp, `dest` can use $<n> for capture groups
 type RedirectRule = { from: string | RegExp; dest: string };
 
-export default [] as RedirectRule[];
+export default [
+  {
+    from: "develop/items/custom-item-groups",
+    dest: "develop/items/custom-creative-tabs",
+  },
+  {
+    from: "versions/1.21.10/develop/items/custom-item-groups",
+    dest: "versions/1.21.10/develop/items/custom-creative-tabs",
+  },
+] as RedirectRule[];
