27600648183f48af1ca36e21fe3a9b8348edb717
Miroma <its.miroma@proton.me>
2026-01-14 17:14:08 +0100

    redirect: item-group -> creative-tab

--- .vitepress/redirects.ts
+++ .vitepress/redirects.ts
@@ -5,4 +5,14 @@ export default [
     from: /^(.*)((?<=^|\/)index)?\.html$/,
     dest: "$1",
   },
+  {
+    from: "develop/items/custom-item-groups",
+    dest: "develop/items/custom-creative-tabs",
+    appliesTo: ">=1.21.10",
+  },
+  {
+    from: "develop/items/custom-creative-tabs",
+    dest: "develop/items/custom-item-groups",
+    appliesTo: "<1.21.10",
+  },
 ] satisfies Fabric.RedirectRules;
