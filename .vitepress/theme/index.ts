7cce682e790e8c3a3b6399319bd205d08bdf49d2
Miroma <its.miroma@proton.me>
2026-01-16 19:04:48 +0100

    implement js-based redirects

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import mediumZoom from "medium-zoom";
-import { type Theme, useData, useRoute } from "vitepress";
+import * as semver from "semver";
+import { inBrowser, type Theme, useData, useRouter } from "vitepress";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import DefaultTheme from "vitepress/theme";
 import { h, nextTick, onMounted, watch } from "vue";
 
+import redirects from "../redirects";
 import AuthorsComponent from "./components/AuthorsComponent.vue";
 import BannerComponent from "./components/BannerComponent.vue";
 import ChoiceComponent from "./components/ChoiceComponent.vue";
@@ -34,7 +36,7 @@ export default {
     app.component("VersionSwitcher", VersionSwitcher);
   },
   Layout: () => {
-    const { page, frontmatter } = useData();
+    const { page, frontmatter, theme } = useData();
 
     const children = {
       "doc-before": () => [
@@ -43,26 +45,55 @@ export default {
         h(VersionReminder),
       ],
       "aside-outline-after": () => [h(VersionReminder), h(AuthorsComponent)],
-      "layout-top": () => h(BannerComponent),
     };
 
+    if (theme.value.env !== "github") {
+      (children as any)["layout-top"] = () => h(BannerComponent);
+    }
+
     if (page.value.isNotFound) {
+      // TODO: set link rel=canonical, meta http-equiv=refresh
       (children as any)["not-found"] = () => h(NotFoundComponent);
     }
 
     return h(DefaultTheme.Layout, null, children);
   },
   setup: () => {
-    const route = useRoute();
-    const initZoom = () => {
-      mediumZoom(".main img", { background: "var(--vp-c-bg)" });
-    };
-    onMounted(() => {
-      initZoom();
-    });
+    const data = useData();
+    const router = useRouter();
+
+    const initZoom = () => mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+    onMounted(() => initZoom());
     watch(
-      () => route.path,
+      () => router.route.path,
       () => nextTick(() => initZoom())
+    );
+
+    watch(
+      () => data.page.value.isNotFound,
+      (isNotFound) => {
+        if (!isNotFound || !inBrowser) return;
+
+        const split = router.route.path.toLowerCase().split("/").slice(1);
+        const locale = /^..[-_]..$/.test(split[0]) ? split.shift()!.replace("-", "_") : undefined;
+        const version = /^[0-9]+\.[0-9]+/.test(split[0]) ? split.shift()! : undefined;
+        let theRest = split.join("/");
+
+        const latestVersion =
+          data.site.value.locales.root.themeConfig.nav.at(-1).props.versioningPlugin.latestVersion;
+        for (const r of redirects) {
+          if (!semver.satisfies(version ?? latestVersion, r.appliesTo ?? "*")) continue;
+          if (r.from === theRest) theRest = r.dest;
+          if (r.from instanceof RegExp) theRest = theRest.replace(r.from, r.dest);
+        }
+
+        const transformed = ["", locale, version, theRest].filter((s) => s !== undefined).join("/");
+
+        if (router.route.path !== transformed) {
+          nextTick(() => router.go(transformed));
+        }
+      },
+      { immediate: true }
     );
   },
 } satisfies Theme;
