abf329b2bd9f2c30c5112b01cb01898ee16e8391
Cassian <cassian@godsted.com>
2026-01-28 19:33:48 -0500

    Rename DrawContext to GuiGraphics

--- reference/latest/src/client/java/com/example/docs/rendering/DrawContextExampleScreen.java
+++ reference/latest/src/client/java/com/example/docs/rendering/DrawContextExampleScreen.java
@@ -14,8 +14,8 @@ public class DrawContextExampleScreen extends Screen {
 	}
 
 	@Override
-	public void render(GuiGraphics context, int mouseX, int mouseY, float delta) {
-		super.render(context, mouseX, mouseY, delta);
+	public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+		super.render(graphics, mouseX, mouseY, delta);
 
 		// :::1
 		int rectangleX = 10;
@@ -23,18 +23,18 @@ public class DrawContextExampleScreen extends Screen {
 		int rectangleWidth = 100;
 		int rectangleHeight = 50;
 		// x1, y1, x2, y2, color
-		context.fill(rectangleX, rectangleY, rectangleX + rectangleWidth, rectangleY + rectangleHeight, 0xFF0000FF);
+		graphics.fill(rectangleX, rectangleY, rectangleX + rectangleWidth, rectangleY + rectangleHeight, 0xFF0000FF);
 		// :::1
 
 		// :::2
 		// x, y, width, height, color
-		context.renderOutline(rectangleX, rectangleY, rectangleWidth, rectangleHeight, 0xFFFF0000);
+		graphics.renderOutline(rectangleX, rectangleY, rectangleWidth, rectangleHeight, 0xFFFF0000);
 		// :::2
 
 		// :::3
 		// Let's split the rectangle in half using a green line.
 		// x, y1, y2, color
-		context.vLine(rectangleX + rectangleWidth / 2, rectangleY, rectangleY + rectangleHeight, 0xFF00FF00);
+		graphics.vLine(rectangleX + rectangleWidth / 2, rectangleY, rectangleY + rectangleHeight, 0xFF00FF00);
 		// :::3
 
 		// :::4
@@ -47,32 +47,32 @@ public class DrawContextExampleScreen extends Screen {
 		int scissorRegionHeight = this.height - 40;
 
 		// x1, y1, x2, y2
-		context.enableScissor(scissorRegionX, scissorRegionY, scissorRegionX + scissorRegionWidth, scissorRegionY + scissorRegionHeight);
+		graphics.enableScissor(scissorRegionX, scissorRegionY, scissorRegionX + scissorRegionWidth, scissorRegionY + scissorRegionHeight);
 
 		// Let's fill the entire screen with a color gradient, it should only be visible in the scissor region.
 		// x1, y1, x2, y2, color1, color2
-		context.fillGradient(0, 0, this.width, this.height, 0xFFFF0000, 0xFF0000FF);
+		graphics.fillGradient(0, 0, this.width, this.height, 0xFFFF0000, 0xFF0000FF);
 
 		// Disable the scissor region.
-		context.disableScissor();
+		graphics.disableScissor();
 		// :::4
 
 		// :::5
 		Identifier texture = Identifier.fromNamespaceAndPath("minecraft", "textures/block/deepslate.png");
 		// renderLayer, texture, x, y, u, v, width, height, textureWidth, textureHeight
-		context.blit(RenderPipelines.GUI_TEXTURED, texture, 90, 90, 0, 0, 16, 16, 16, 16);
+		graphics.blit(RenderPipelines.GUI_TEXTURED, texture, 90, 90, 0, 0, 16, 16, 16, 16);
 		// :::5
 
 		// :::6
 		Identifier texture2 = Identifier.fromNamespaceAndPath(ExampleMod.MOD_ID, "textures/gui/test-uv-drawing.png");
 		int u = 10, v = 13, regionWidth = 14, regionHeight = 14;
 		// renderLayer, texture, x, y, width, height, u, v, regionWidth, regionHeight, textureWidth, textureHeight
-		context.blit(RenderPipelines.GUI_TEXTURED, texture2, 90, 190, 14, 14, u, v, regionWidth, regionHeight, 256, 256);
+		graphics.blit(RenderPipelines.GUI_TEXTURED, texture2, 90, 190, 14, 14, u, v, regionWidth, regionHeight, 256, 256);
 		// :::6
 
 		// :::7
 		// TextRenderer, text (string, or Text object), x, y, color, shadow
-		context.drawString(minecraft.font, "Hello, world!", 10, 200, 0xFFFFFFFF, false);
+		graphics.drawString(minecraft.font, "Hello, world!", 10, 200, 0xFFFFFFFF, false);
 		// :::7
 	}
 }
