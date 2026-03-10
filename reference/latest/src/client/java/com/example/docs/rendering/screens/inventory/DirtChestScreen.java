7628be0747d96cf17525845aac5cd1472c19a1e8
Tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2026-03-10 21:37:04 +0100

    more parameter renames
    Signed-off-by: Tenneb22 <126985099+Tenneb22@users.noreply.github.com>

--- reference/latest/src/client/java/com/example/docs/rendering/screens/inventory/DirtChestScreen.java
+++ reference/latest/src/client/java/com/example/docs/rendering/screens/inventory/DirtChestScreen.java
@@ -26,7 +26,7 @@ public class DirtChestScreen extends AbstractContainerScreen<DirtChestMenu> {
 	}
 
 	@Override
-	protected void renderBg(GuiGraphics graphics, float f, int i, int j) {
+	protected void renderBg(GuiGraphics graphics, float delta, int mouseX, int mouseY) {
 		int xo = (this.width - this.imageWidth) / 2;
 		int yo = (this.height - this.imageHeight) / 2;
 		graphics.blit(RenderPipelines.GUI_TEXTURED, CONTAINER_TEXTURE, xo, yo, 0.0F, 0.0F, this.imageWidth, this.imageHeight, BACKGROUND_TEXTURE_WIDTH, BACKGROUND_TEXTURE_HEIGHT);
