299aa39fcb64f4edc6c9ff76ae921b633736f6a1
Miroma <its.miroma@proton.me>
2026-03-21 17:42:51 +0100

    redirect: /latestVersion/* -> /*

--- .vitepress/redirects.ts
+++ .vitepress/redirects.ts
@@ -1,6 +1,12 @@
+import latestVersion from "virtual:fabric-docs:latest-version";
+
 export default [
   {
     from: /((?<=^|[/])index)?[.]html$/,
+    dest: "",
+  },
+  {
+    from: new RegExp(`^${latestVersion}([/]|$)`),
     dest: "",
   },
 ] satisfies { from: RegExp; dest: string }[];
