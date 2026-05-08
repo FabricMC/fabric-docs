ddfa55c7e590cec9a18af617abf41dbc04f5264e
CelDaemon <celdaemon@voidgroup.net>
2026-05-08 20:01:03 +0200

    Fix checkstyle

--- reference/latest/src/main/java/com/example/docs/codec/CoolBeansClass.java
+++ reference/latest/src/main/java/com/example/docs/codec/CoolBeansClass.java
@@ -1,25 +1,25 @@
 package com.example.docs.codec;
 
-import com.mojang.serialization.Codec;
+import java.util.List;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.Item;
 
-import java.util.List;
-
 // #region bean-class
 public class CoolBeansClass {
 	// #endregion bean-class
 	// #region bean-codec
 	public static final Codec<CoolBeansClass> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			// Up to 16 fields can be declared here
 			Codec.INT.fieldOf("beans_amount").forGetter(CoolBeansClass::getBeansAmount),
 			Item.CODEC.fieldOf("bean_type").forGetter(CoolBeansClass::getBeanType),
 			BlockPos.CODEC.listOf().fieldOf("bean_positions").forGetter(CoolBeansClass::getBeanPositions)
-			// Up to 16 fields can be declared here
-	).apply(instance, CoolBeansClass::new));
+	)
+			.apply(instance, CoolBeansClass::new));
 	// #endregion bean-codec
 	// #region bean-class
 	private final int beansAmount;
@@ -38,9 +38,11 @@ public class CoolBeansClass {
 	public int getBeansAmount() {
 		return this.beansAmount;
 	}
+
 	public Holder<Item> getBeanType() {
 		return this.beanType;
 	}
+
 	public List<BlockPos> getBeanPositions() {
 		return this.beanPositions;
 	}
--- reference/latest/src/main/java/com/example/docs/codec/ListNode.java
+++ reference/latest/src/main/java/com/example/docs/codec/ListNode.java
@@ -3,5 +3,6 @@ package com.example.docs.codec;
 import java.util.Optional;
 
 // #region node-record
-public record ListNode(int value, Optional<ListNode> next) {}
+public record ListNode(int value, Optional<ListNode> next) {
+}
 // #endregion node-record
