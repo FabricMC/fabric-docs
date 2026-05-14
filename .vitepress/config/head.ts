b4e0efddbc3206ed9ff33c847467cc642000804e
Miroma <its.miroma@proton.me>
2026-05-14 22:36:07 +0200

    chore: complete and reverse versionMap

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -15,11 +15,39 @@ type NewHeadContext = {
 };
 
 const _getNewHead = (context: NewHeadContext): string | [string, Record<string, string>][] => {
-  const versionMap = {
-    "1.21.10": "1.21.11",
-    "26.1.0": "26.1.2",
+  const versionMap: Record<string, string> = {
+    "26.1.2": "26.1.2",
     "26.1.1": "26.1.2",
-    [context.latestVersion]: "",
+    "26.1.0": "26.1.2",
+
+    "1.21.11": "1.21.11",
+
+    // TODO: bring back 1.21.10
+    "1.21.10": "1.21.11",
+    "1.21.9": "1.21.11",
+
+    "1.21.8": "1.21.8",
+    "1.21.7": "1.21.8",
+    "1.21.6": "1.21.8",
+
+    // not on the Docs
+    "1.21.5": "1.21.5",
+
+    "1.21.4": "1.21.4",
+
+    // not on the Docs
+    "1.21.3": "1.21.3",
+    "1.21.2": "1.21.3",
+
+    "1.21.1": "1.21.1",
+    "1.21.0": "1.21.1",
+
+    // not on the Docs
+    "1.20.6": "1.20.6",
+    "1.20.5": "1.20.6",
+
+    "1.20.4": "1.20.4",
+    "1.20.3": "1.20.4",
   };
 
   const redirects: { from: RegExp; dest: string }[] = [
@@ -72,7 +100,7 @@ const _getNewHead = (context: NewHeadContext): string | [string, Record<string, 
     : "/";
 
   split[0] = versionMap[split[0]] ?? versionMap[split[0]?.replace(/[.]0$/, "")] ?? split[0];
-  if (!split[0]) split.shift();
+  if (!split[0] || split[0] === context.latestVersion) split.shift();
 
   const seenPaths = new Set([split.join("/")]);
   const newPath = redirects.reduce((currentPath, rule) => {
