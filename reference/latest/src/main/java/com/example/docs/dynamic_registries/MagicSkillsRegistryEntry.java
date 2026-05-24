4f47ebed2bce97cc58faa7f4e51ab7db74e37347
Jimmy474 <98521564+Jimmy474@users.noreply.github.com>
2026-05-24 21:46:23 +0530

    renamed codec fields to use snake_case

--- reference/latest/src/main/java/com/example/docs/dynamic_registries/MagicSkillsRegistryEntry.java
+++ reference/latest/src/main/java/com/example/docs/dynamic_registries/MagicSkillsRegistryEntry.java
@@ -14,8 +14,8 @@ public record MagicSkillsRegistryEntry(String name, int manaCost, Optional<Cache
 	public static final Codec<MagicSkillsRegistryEntry> CODEC = RecordCodecBuilder.create(instance ->
 		instance.group(
 			Codec.STRING.fieldOf("name").forGetter(MagicSkillsRegistryEntry::name),
-			Codec.INT.fieldOf("manaCost").forGetter(MagicSkillsRegistryEntry::manaCost),
-			CacheableFunction.CODEC.optionalFieldOf("onUseMcFunction").forGetter(MagicSkillsRegistryEntry::onUseMcFunction)
+			Codec.INT.fieldOf("mana_cost").forGetter(MagicSkillsRegistryEntry::manaCost),
+			CacheableFunction.CODEC.optionalFieldOf("on_use_mc_function").forGetter(MagicSkillsRegistryEntry::onUseMcFunction)
 		).apply(instance, MagicSkillsRegistryEntry::new)
 	);
 	// #endregion main
@@ -26,7 +26,7 @@ public record MagicSkillsRegistryEntry(String name, int manaCost, Optional<Cache
 	public static final Codec<MagicSkillsRegistryEntry> CLIENT_CODEC = RecordCodecBuilder.create(instance ->
 		instance.group(
 			Codec.STRING.fieldOf("name").forGetter(MagicSkillsRegistryEntry::name),
-			Codec.INT.fieldOf("manaCost").forGetter(MagicSkillsRegistryEntry::manaCost)
+			Codec.INT.fieldOf("mana_cost").forGetter(MagicSkillsRegistryEntry::manaCost)
 		).apply(instance, (name,manaCost) -> new MagicSkillsRegistryEntry(name, manaCost, Optional.empty()))
 	);
 	// #region main
