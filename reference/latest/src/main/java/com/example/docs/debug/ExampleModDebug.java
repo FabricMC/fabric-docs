f5bbe47a358a06151c318438341801a084962d66
Cassian <cassian@godsted.com>
2026-06-01 19:58:40 -0400

    Quick fix

--- reference/latest/src/main/java/com/example/docs/debug/ExampleModDebug.java
+++ reference/latest/src/main/java/com/example/docs/debug/ExampleModDebug.java
@@ -4,22 +4,22 @@ import net.fabricmc.api.ModInitializer;
 
 import com.example.docs.ExampleMod;
 
-// #region problems:basic-logger-definition
+// #region problems--basic-logger-definition
 public class ExampleModDebug implements ModInitializer {
-	// #endregion problems:basic-logger-definition
+	// #endregion problems--basic-logger-definition
 
 	@Override
 	public void onInitialize() {
-		// #region problems:debug-logging
+		// #region problems--debug-logging
 		ExampleMod.LOGGER.debug("Debug logging is enabled");
-		// #endregion problems:debug-logging
+		// #endregion problems--debug-logging
 
-		// #region problems:log-levels
+		// #region problems--log-levels
 		ExampleMod.LOGGER.debug("Debug message for development...");
 		ExampleMod.LOGGER.info("Neutral, informative text...");
 		ExampleMod.LOGGER.warn("Non-critical issues..."); // [!code warning]
 		ExampleMod.LOGGER.error("Critical exceptions, bugs..."); // [!code error]
-		// #endregion problems:log-levels
+		// #endregion problems--log-levels
 	}
 
 	// #region problems--basic-logger-definition
