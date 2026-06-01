85a5f7c236340ccd3eaca0e31517f8bb7cb1d4a8
Cassian <cassian@godsted.com>
2026-06-01 13:02:19 -0400

    Remove use of Fabric API implementation
    Co-Authored-By: Celeste <celdaemon@voidgroup.net>

--- reference/latest/src/client/java/com/example/docs/datagen/internal/ExampleModResourceConditionProvider.java
+++ reference/latest/src/client/java/com/example/docs/datagen/internal/ExampleModResourceConditionProvider.java
@@ -6,13 +6,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
@@ -22,25 +20,12 @@ import net.minecraft.world.level.block.Blocks;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceCondition;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditionType;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
-import net.fabricmc.fabric.impl.resource.conditions.DefaultResourceConditionTypes;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.AllModsLoadedResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.AndResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.AnyModsLoadedResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.FeaturesEnabledResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.NotResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.OrResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.RegistryContainsResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.TagsPopulatedResourceCondition;
-import net.fabricmc.fabric.impl.resource.conditions.conditions.TrueResourceCondition;
 
 import com.example.docs.ExampleMod;
 import com.example.docs.conditions.DateMatchesResourceCondition;
-import com.example.docs.conditions.ModResourceConditions;
 import com.example.docs.datagen.ExampleModItemTagProvider;
 
-@SuppressWarnings("all")
 public class ExampleModResourceConditionProvider implements DataProvider {
 	private final FabricPackOutput output;
 
@@ -53,46 +38,38 @@ public class ExampleModResourceConditionProvider implements DataProvider {
 	);
 
 	private static void encodeResourceConditions(BiConsumer<String, JsonElement> consumer) {
-		acceptCondition(consumer, DefaultResourceConditionTypes.TRUE, new TrueResourceCondition());
-		acceptCondition(consumer, DefaultResourceConditionTypes.NOT, new NotResourceCondition(ResourceConditions.alwaysTrue()));
-		acceptCondition(consumer, DefaultResourceConditionTypes.OR, new OrResourceCondition(List.of(ResourceConditions.alwaysTrue(), new NotResourceCondition(ResourceConditions.alwaysTrue()))));
-		acceptCondition(consumer, DefaultResourceConditionTypes.AND, new AndResourceCondition(List.of(ResourceConditions.alwaysTrue(), new NotResourceCondition(ResourceConditions.alwaysTrue()))));
-		acceptCondition(consumer, DefaultResourceConditionTypes.ANY_MODS_LOADED, new AnyModsLoadedResourceCondition(List.of("example-mod", "another-mod")));
-		acceptCondition(consumer, DefaultResourceConditionTypes.ALL_MODS_LOADED, new AllModsLoadedResourceCondition(List.of("example-mod", "another-mod")));
-		acceptCondition(consumer, DefaultResourceConditionTypes.TAGS_POPULATED, new TagsPopulatedResourceCondition(ExampleModItemTagProvider.SMELLY_ITEMS));
-		acceptCondition(consumer, DefaultResourceConditionTypes.FEATURES_ENABLED, new FeaturesEnabledResourceCondition(FeatureFlags.VANILLA, FeatureFlags.MINECART_IMPROVEMENTS));
-		acceptCondition(consumer, DefaultResourceConditionTypes.REGISTRY_CONTAINS, new RegistryContainsResourceCondition(Blocks.COBBLESTONE.builtInRegistryHolder().key()));
-		acceptCondition(consumer, ModResourceConditions.DATE_MATCHES, new DateMatchesResourceCondition(4, 1));
+		acceptCondition(consumer, ResourceConditions.alwaysTrue());
+		acceptCondition(consumer, ResourceConditions.not(ResourceConditions.alwaysTrue()));
+		acceptCondition(consumer, ResourceConditions.or(ResourceConditions.alwaysTrue(), ResourceConditions.not(ResourceConditions.alwaysTrue())));
+		acceptCondition(consumer, ResourceConditions.and(ResourceConditions.alwaysTrue(), ResourceConditions.not(ResourceConditions.alwaysTrue())));
+		acceptCondition(consumer, ResourceConditions.anyModsLoaded("example-mod", "another-mod"));
+		acceptCondition(consumer, ResourceConditions.allModsLoaded("example-mod", "another-mod"));
+		acceptCondition(consumer, ResourceConditions.tagsPopulated(ExampleModItemTagProvider.SMELLY_ITEMS));
+		acceptCondition(consumer, ResourceConditions.featuresEnabled(FeatureFlags.VANILLA, FeatureFlags.MINECART_IMPROVEMENTS));
+		acceptCondition(consumer, ResourceConditions.registryContains(BuiltInRegistries.BLOCK.getResourceKey(Blocks.COBBLESTONE).orElseThrow()));
+		acceptCondition(consumer, new DateMatchesResourceCondition(4, 1));
 	}
 
-	private static <T extends ResourceCondition> void acceptCondition(BiConsumer<String, JsonElement> consumer, ResourceConditionType<T> type, T condition) {
-		consumer.accept(type.id().getPath(), encodeConditions(type, condition));
+	private static <T extends ResourceCondition> void acceptCondition(BiConsumer<String, JsonElement> consumer, T condition) {
+		consumer.accept(condition.getType().id().getPath(), encodeConditions(condition));
 	}
 
-	private static <T extends ResourceCondition> JsonElement encodeConditions(ResourceConditionType<T> type, T condition) {
+	private static <T extends ResourceCondition> JsonElement encodeConditions(T condition) {
 		JsonObject finalConditions = new JsonObject();
-		JsonArray conditions = new JsonArray();
-		JsonObject encodedCondition = encodeCondition(type, condition);
-		conditions.add(encodedCondition);
-		finalConditions.add(ResourceConditions.CONDITIONS_KEY, conditions);
+		JsonElement encodedCondition = encodeCondition(condition);
+		finalConditions.add(ResourceConditions.CONDITIONS_KEY, encodedCondition);
 
 		return finalConditions;
 	}
 
-	private static <T extends ResourceCondition> JsonObject encodeCondition(ResourceConditionType<T> type, T trueResourceCondition) {
-		JsonObject encodedCondition = type.codec().codec().encodeStart(JsonOps.INSTANCE, trueResourceCondition).getOrThrow().getAsJsonObject();
-		encodedCondition.add("condition", new JsonPrimitive(type.id().toString()));
-		return encodedCondition;
+	private static <T extends ResourceCondition> JsonElement encodeCondition(T condition) {
+		return ResourceCondition.LIST_CODEC.encodeStart(JsonOps.INSTANCE, List.of(condition)).getOrThrow();
 	}
 
 	private static void collect(BiConsumer<String, JsonElement> consumer) {
 		for (final var submitter : SUBMITTERS) {
 			submitter.accept(consumer);
 		}
-	}
-
-	private static <T> JsonElement encode(Codec<T> codec, T value) {
-		return codec.encodeStart(JsonOps.INSTANCE, value).getOrThrow();
 	}
 
 	@Override
