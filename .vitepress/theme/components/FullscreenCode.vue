39902101b7bf7b9096f0103f554c65eeeb4c08e9
Miroma <its.miroma@proton.me>
2026-06-10 09:17:52 +0200

    refactor

--- .vitepress/theme/components/FullscreenCode.vue
+++ .vitepress/theme/components/FullscreenCode.vue
@@ -10,122 +10,85 @@ const data = useData();
 const options = computed(() => data.theme.value.code as Fabric.CodeOptions);
 
 const dialog = ref<HTMLDialogElement>();
-const tabs = ref<HTMLDivElement>();
-const originalCodeGroup = ref<HTMLDivElement>();
+const originalCopyButton = ref<HTMLButtonElement>();
+
+const originalTabs = ref<HTMLInputElement[]>([]);
+const originalCodeBlocks = ref<HTMLDivElement[]>([]);
+
 const isClosing = ref(false);
 const isWrapped = ref(false);
 const isCopied = ref(false);
-const isInTransition = ref(false);
 
-const processedCodeBlocks = new WeakSet<HTMLDivElement>();
-let originalCopyButton: HTMLButtonElement | null = null;
-
-const handleEnterFullscreen = async (originalCodeBlock: HTMLDivElement) => {
-  await nextTick();
+const loadCodeBlock = (originalCodeBlock: HTMLDivElement) => {
   if (!dialog.value) return;
 
-  originalCodeGroup.value = originalCodeBlock.closest(".vp-code-group") as HTMLDivElement;
-  await nextTick();
+  originalCopyButton.value = //
+    originalCodeBlock.querySelector<HTMLButtonElement>("button.copy:not(.fullscreen)") ?? undefined;
 
-  if (tabs.value && originalCodeGroup.value) {
-    tabs.value.innerHTML = "";
-    const codeGroupTabs = originalCodeGroup.value.querySelectorAll(
-      ".tabs > input[type='radio']"
-    ) as NodeListOf<HTMLInputElement>;
-    let index = 0;
-    for (const tab of codeGroupTabs) {
-      const newInput = document.createElement("input");
-      newInput.type = "radio";
-      newInput.name = `code-group-tabs`;
-      newInput.id = `code-group-tab-${index}`;
-      newInput.checked = tab.checked;
-      newInput.setAttribute("index", index.toString());
-      newInput.onchange = handleTabChange;
+  const clonedCodeBlock = originalCodeBlock.cloneNode(true) as HTMLDivElement;
+  clonedCodeBlock.querySelector<HTMLDivElement>("div.line-numbers-wrapper")?.remove();
+  clonedCodeBlock.querySelectorAll<HTMLButtonElement>("button.copy").forEach((b) => b.remove());
 
-      const newLabel = document.createElement("label");
-      newLabel.htmlFor = `code-group-tab-${index}`;
-      newLabel.textContent = tab.labels?.[0]?.textContent ?? "text";
-      newLabel.setAttribute("data-title", newLabel.textContent);
+  dialog.value.querySelector<HTMLDivElement>("div.slot")?.replaceChildren(clonedCodeBlock);
+};
 
-      tabs.value.append(newInput);
-      tabs.value.append(newLabel);
-      index++;
-    }
+const handleEnterFullscreen = (originalCodeBlock: HTMLDivElement) => {
+  if (!dialog.value) return;
+
+  const originalCodeGroup = originalCodeBlock.closest<HTMLDivElement>("div.vp-code-group");
+  if (originalCodeGroup) {
+    originalTabs.value = [
+      ...originalCodeGroup.querySelectorAll<HTMLInputElement>("div.tabs > input[type='radio']"),
+    ];
+    originalCodeBlocks.value = [
+      ...originalCodeGroup.querySelectorAll<HTMLDivElement>("div.blocks div[class*='language-']"),
+    ];
   }
 
   loadCodeBlock(originalCodeBlock);
   dialog.value.showModal();
 };
 
-const handleTabChange = (event: Event) => {
-  if (!originalCodeGroup.value) return;
-  const target = event.target as HTMLInputElement;
-  if (!target.checked) return;
-  const newCodeBlock = originalCodeGroup.value.querySelector(".blocks")?.children[
-    Number(target.getAttribute("index"))
-  ] as HTMLDivElement;
-  loadCodeBlock(newCodeBlock);
-};
+const handleTabChange = (index: number) => {
+  if (!originalTabs.value[index] || !originalCodeBlocks.value[index]) return;
 
-const loadCodeBlock = (codeBlock: HTMLDivElement) => {
-  if (!dialog.value) return;
-
-  originalCopyButton = codeBlock.querySelector<HTMLButtonElement>("button.copy:not(.fullscreen)");
-  const clonedCodeBlock = codeBlock.cloneNode(true) as HTMLDivElement;
-  clonedCodeBlock.querySelector<HTMLDivElement>("div.line-numbers-wrapper")?.remove();
-  clonedCodeBlock.querySelectorAll<HTMLButtonElement>("button.copy").forEach((b) => b.remove());
-
-  const slot = dialog.value?.querySelector<HTMLDivElement>("div.slot");
-  const oldSlot = slot?.querySelector<HTMLDivElement>("div.old-slot-content");
-
-  if (!slot || !oldSlot) return;
-
-  const currentContent = slot.firstElementChild;
-
-  if (originalCodeGroup.value && currentContent && currentContent !== oldSlot) {
-    oldSlot.replaceChildren(currentContent);
-
-    isInTransition.value = true;
-
-    oldSlot.addEventListener(
-      "transitionend",
-      () => {
-        isInTransition.value = false;
-        oldSlot.replaceChildren();
-      },
-      { once: true }
-    );
-  }
-
-  slot.prepend(clonedCodeBlock);
+  originalTabs.value[index].click();
+  loadCodeBlock(originalCodeBlocks.value[index]);
 };
 
 const handleCopy = (event: Event) => {
-  (event.currentTarget as HTMLButtonElement).blur();
-  originalCopyButton?.click();
+  if (event.currentTarget instanceof HTMLButtonElement) event.currentTarget.blur();
+  originalCopyButton.value?.click();
   isCopied.value = true;
   setTimeout(() => (isCopied.value = false), 2000);
 };
 
 const handleWrap = (event: Event) => {
-  (event.currentTarget as HTMLButtonElement).blur();
+  if (event.currentTarget instanceof HTMLButtonElement) event.currentTarget.blur();
   isWrapped.value = !isWrapped.value;
 };
 
-const handleExitFullscreen = (event: Event) => {
-  (event.currentTarget as HTMLButtonElement)?.blur();
-  dialog.value?.focus();
-  dialog.value?.blur();
+const handleExitFullscreen = (event?: Event) => {
+  if (event?.currentTarget instanceof HTMLButtonElement) event.currentTarget.blur();
+  if (!dialog.value?.open) return;
+
+  dialog.value.focus();
+  dialog.value.blur();
   isClosing.value = true;
 
   const onAnimationend = () => {
     isClosing.value = false;
+    originalCopyButton.value = undefined;
+    originalTabs.value = [];
+    originalCodeBlocks.value = [];
+
     dialog.value?.close();
+    dialog.value?.querySelector<HTMLDivElement>("div.slot")?.replaceChildren();
   };
 
   if (prefersReducedMotion.value === "reduce") return onAnimationend();
 
-  dialog.value?.addEventListener("animationend", onAnimationend, { once: true });
+  dialog.value.addEventListener("animationend", onAnimationend, { once: true });
 };
 
 onContentUpdated(() =>
@@ -133,43 +96,52 @@ onContentUpdated(() =>
     if (!dialog.value) return;
 
     await loadIcon("lucide:maximize-2");
-    dialog.value.close();
+    handleExitFullscreen();
 
     document.documentElement.style.setProperty(
       "--vp-code-copy-copied-text-content",
       JSON.stringify(options.value.copied)
     );
 
-    const enterFullscreenIconData = getIcon("lucide:maximize-2");
-    const enterFullscreenIcon = enterFullscreenIconData
-      ? `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">${enterFullscreenIconData.body}</svg>`
-      : "";
+    const getSvgIcon = (name: string) => {
+      const iconData = getIcon(name);
+      if (!iconData) return "";
+
+      return `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">${iconData.body}</svg>`;
+    };
+
+    const enterFullscreenIcon = getSvgIcon("lucide:maximize-2");
 
     const codeBlocks = //
-      document.querySelectorAll<HTMLDivElement>(".vp-doc:not(.slot) div[class*='language-']");
+      document.querySelectorAll<HTMLDivElement>("div.vp-doc:not(.slot) div[class*='language-']");
 
     for (const codeBlock of codeBlocks) {
-      if (processedCodeBlocks.has(codeBlock)) continue;
+      const originalCopyButton = //
+        codeBlock.querySelector<HTMLButtonElement>("button.copy:not(.fullscreen)");
+      if (!originalCopyButton) continue;
 
-      const copyButton = codeBlock.querySelector<HTMLButtonElement>("button.copy:not(.fullscreen)");
-      if (copyButton) {
-        copyButton.title = options.value.copy;
-        copyButton.setAttribute("aria-label", options.value.copy);
-      }
+      originalCopyButton.title = options.value.copy;
+      originalCopyButton.setAttribute("aria-label", options.value.copy);
 
       const enterFullscreenButton = document.createElement("button");
       enterFullscreenButton.title = options.value.enterFullscreen;
       enterFullscreenButton.setAttribute("aria-label", options.value.enterFullscreen);
       enterFullscreenButton.className = "copy fullscreen";
       enterFullscreenButton.innerHTML = enterFullscreenIcon;
-      enterFullscreenButton.addEventListener("click", (e) => {
-        e.stopImmediatePropagation();
-        enterFullscreenButton.blur();
+      enterFullscreenButton.addEventListener("click", (event) => {
+        if (!(event.currentTarget instanceof HTMLButtonElement)) return;
+
+        event.stopImmediatePropagation();
+        event.currentTarget.blur();
+
+        const codeBlock = event.currentTarget.closest<HTMLDivElement>("div[class*='language-']");
+        if (!codeBlock) return;
+
         handleEnterFullscreen(codeBlock);
       });
 
+      codeBlock.querySelector<HTMLButtonElement>("button.copy.fullscreen")?.remove();
       codeBlock.prepend(enterFullscreenButton);
-      processedCodeBlocks.add(codeBlock);
     }
   })
 );
@@ -183,11 +155,21 @@ onUnmounted(() => dialog.value?.close());
     id="fullscreen"
     aria-modal="true"
     :class="{ closing: isClosing }"
-    @click.self="handleExitFullscreen"
     @cancel.prevent="handleExitFullscreen"
   >
     <div class="toolbar">
-      <div v-if="originalCodeGroup" ref="tabs" class="tabs" />
+      <div v-if="originalTabs.length" class="tabs">
+        <template v-for="(tab, i) in originalTabs" :key="i">
+          <input
+            type="radio"
+            :name="`dialog-fullscreen-${tab.name}`"
+            :id="`dialog-fullscreen-${tab.id}`"
+            :checked="tab.checked"
+            @change="handleTabChange(i)"
+          />
+          <label :for="`dialog-fullscreen-${tab.id}`">{{ tab.labels?.[0]?.textContent }}</label>
+        </template>
+      </div>
       <button
         class="copy"
         :class="{ copied: isCopied }"
@@ -195,7 +177,7 @@ onUnmounted(() => dialog.value?.close());
         :aria-label="options.copy"
         @click="handleCopy"
       >
-        <span class="copied-label">{{ options.copied }}</span>
+        <span>{{ options.copied }}</span>
         <Icon v-if="isCopied" icon="lucide:clipboard-check" />
         <Icon v-else icon="lucide:clipboard" />
       </button>
@@ -212,41 +194,31 @@ onUnmounted(() => dialog.value?.close());
         <Icon icon="lucide:minimize-2" />
       </button>
     </div>
-    <div class="slot vp-doc" :class="{ wrapped: isWrapped, grouped: originalCodeGroup }">
-      <div
-        class="old-slot-content vp-ignore"
-        :class="{ wrapped: isWrapped, grouped: originalCodeGroup, inTransition: isInTransition }"
-      />
-    </div>
+    <div class="slot vp-doc" :class="{ wrapped: isWrapped }" />
   </dialog>
 </template>
 
 <style scoped>
 dialog#fullscreen {
+  height: 100vh;
+  width: 100vw;
   max-height: none;
   max-width: none;
   border: none;
   overflow: hidden;
   background: transparent;
   animation:
-    fadeIn 0.3s ease-in,
+    fade-in 0.3s ease-in,
     scale-in 0.3s ease;
 
   &::backdrop {
     background-color: var(--vp-c-bg);
-    animation: fadeIn 0.3s ease;
+    animation: fade-in 0.3s ease;
   }
 
   &[open] {
     display: flex;
     flex-direction: column;
-  }
-
-  &,
-  &::backdrop {
-    height: 100vh;
-    width: 100vw;
-    will-change: transform, opacity;
   }
 
   &[open].closing {
@@ -265,6 +237,7 @@ dialog#fullscreen {
   from {
     opacity: 0;
   }
+
   to {
     opacity: 1;
   }
@@ -274,6 +247,7 @@ dialog#fullscreen {
   from {
     opacity: 1;
   }
+
   to {
     opacity: 0;
   }
@@ -283,6 +257,7 @@ dialog#fullscreen {
   from {
     transform: scale(0.9);
   }
+
   to {
     transform: scale(1);
   }
@@ -292,6 +267,7 @@ dialog#fullscreen {
   from {
     transform: scale(1);
   }
+
   to {
     transform: scale(0.9);
   }
@@ -313,19 +289,16 @@ div.toolbar {
     font-weight: 500;
     color: var(--vp-code-tab-text-color);
     background-color: var(--vp-code-tab-bg);
-    box-shadow:
-      inset 0 -1px var(--vp-code-block-divider-color),
-      0 1px var(--vp-code-tab-bg);
     border: 1px solid var(--vp-c-divider);
     border-bottom: none;
     border-radius: 8px 8px 0 0;
     white-space: nowrap;
-    overflow: auto hidden;
+    overflow-x: auto;
+    overflow-y: hidden;
     z-index: 10;
-    will-change: width;
 
     &::-webkit-scrollbar {
-      height: 8px;
+      scrollbar-width: thin;
     }
 
     &::-webkit-scrollbar-track {
@@ -358,7 +331,6 @@ div.toolbar {
         height: 2px;
         background-color: transparent;
         transition: background-color 0.25s;
-        will-change: background-color;
       }
     }
 
@@ -372,6 +344,13 @@ div.toolbar {
           background-color: var(--vp-code-tab-active-bar-color);
         }
       }
+    }
+  }
+
+  &:has(div.tabs) + :deep(div.slot) {
+    &,
+    > div[class*="language-"] {
+      border-top-left-radius: 0;
     }
   }
 
@@ -397,7 +376,7 @@ div.toolbar {
       background-color: var(--vp-code-copy-code-hover-bg);
     }
 
-    svg {
+    svg.iconify {
       height: 20px;
       width: 20px;
     }
@@ -406,62 +385,43 @@ div.toolbar {
       width: auto;
       min-width: 40px;
       gap: 0;
-      padding-right: 0;
-      transition:
-        gap 0.3s ease,
-        padding-right 0.3s ease;
-      will-change: gap, padding-right;
 
-      & span.copied-label {
-        transition:
-          max-width 0.3s ease,
-          padding 0.3s ease,
-          opacity 0.1s ease-out;
-        will-change: max-width, padding, opacity;
-        height: 100%;
-        max-width: 0;
-        display: inline-flex;
-        align-items: center;
-        padding: 0 0;
-        font-size: 12px;
-        font-weight: 500;
-        white-space: nowrap;
-        color: var(--vp-code-copy-code-active-text);
-        border-radius: 4px 0 0 4px;
-        opacity: 0;
+      svg.iconify {
+        margin-left: 8px;
+        margin-right: 9px;
       }
 
-      &.copied {
-        gap: 9px;
-        padding-right: 9px;
+      & span {
+        display: inline-flex;
+        align-items: center;
+        overflow: hidden;
+        white-space: nowrap;
+        height: 100%;
+        max-width: 0;
+        font-size: 12px;
+        font-weight: 500;
+        color: var(--vp-code-copy-code-active-text);
+        border-right: 1px solid transparent;
+        transition:
+          max-width 0.3s ease,
+          padding-inline 0.3s ease,
+          border-color 0.3s ease;
+      }
 
-        & span.copied-label {
-          max-width: 100px;
-          opacity: 1;
-          border-right: 1px solid var(--vp-code-copy-code-hover-border-color);
-          padding: 0 10px;
-          transition:
-            max-width 0.3s ease,
-            padding 0.3s ease,
-            opacity 0.3s ease-in;
-        }
+      &.copied span {
+        max-width: 100px;
+        padding-inline: 9px;
+        border-right-color: var(--vp-code-copy-code-hover-border-color);
       }
     }
   }
 }
 
-/* :deep is needed because clonedCodeBlock does not have the data-v-****** attribute */
 :deep(div.slot) {
-  position: relative;
   border-radius: 12px;
   border: 1px solid var(--vp-c-divider);
   flex-grow: 1;
   overflow: hidden;
-
-  &.grouped,
-  &.grouped > div[class*="language-"] {
-    border-top-left-radius: 0;
-  }
 
   div[class*="language-"] {
     margin: 0;
@@ -504,7 +464,7 @@ div.toolbar {
         padding-left: 0;
       }
 
-      .line {
+      span.line {
         position: relative;
         padding-left: 56px;
 
@@ -513,7 +473,7 @@ div.toolbar {
         }
       }
 
-      .line::before {
+      span.line::before {
         content: counter(line-counter);
         counter-increment: line-counter;
         display: inline-block;
@@ -530,7 +490,7 @@ div.toolbar {
         border-right: 1px solid var(--vp-code-block-divider-color);
       }
 
-      .line:empty::after {
+      span.line:empty::after {
         content: "\a0";
       }
     }
@@ -539,22 +499,6 @@ div.toolbar {
   &.wrapped div[class*="language-"] pre {
     white-space: pre-wrap;
     overflow-wrap: anywhere;
-  }
-
-  & div.old-slot-content {
-    position: absolute;
-    inset: 0;
-    transform: scale(0);
-    transform-origin: bottom right;
-    opacity: 1;
-    z-index: 2;
-    transition: opacity 0.3s ease;
-    will-change: opacity;
-
-    &.inTransition {
-      transform: scale(1);
-      opacity: 0;
-    }
   }
 }
 </style>
