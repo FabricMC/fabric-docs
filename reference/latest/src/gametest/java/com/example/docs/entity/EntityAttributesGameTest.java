073441234c914393219e7a08a873ec09ca603105
Tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2026-06-13 21:19:17 +0200

    replace vars with explicit types

--- reference/latest/src/gametest/java/com/example/docs/entity/EntityAttributesGameTest.java
+++ reference/latest/src/gametest/java/com/example/docs/entity/EntityAttributesGameTest.java
@@ -7,6 +7,7 @@ import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.level.Level;
 
@@ -20,14 +21,14 @@ public class EntityAttributesGameTest implements CustomTestMethodInvoker {
 	@GameTest
 	void testReadingAttributes(GameTestHelper helper, LivingEntity entity) {
 		// #region reading_entity_attributes
-		var attribute = entity.getAttribute(ModAttributes.AGGRO_RANGE); // returns an `AttributeInstance`
+		AttributeInstance attribute = entity.getAttribute(ModAttributes.AGGRO_RANGE); // returns an `AttributeInstance`
 		// #endregion reading_entity_attributes
 
 		assertNotNull(attribute, helper, Component.literal("Expected AGGRO_RANGE attribute to be present"));
 
 		// #region reading_entity_attributes
-		var value = entity.getAttribute(ModAttributes.AGGRO_RANGE).getValue(); // returns a double with the current value
-		var baseValue = entity.getAttribute(ModAttributes.AGGRO_RANGE).getBaseValue(); // returns a double with the base value
+		double value = attribute.getValue(); // returns a double with the current value
+		double baseValue = attribute.getBaseValue(); // returns a double with the base value
 		// #endregion reading_entity_attributes
 
 		helper.assertValueEqual(8.0, value, "Expected AGGRO_RANGE attribute to have a value of 8.0");
@@ -50,7 +51,7 @@ public class EntityAttributesGameTest implements CustomTestMethodInvoker {
 		);
 		// #endregion modifying_entity_attributes
 
-		helper.assertValueEqual(16.0, entity.getAttribute(ModAttributes.AGGRO_RANGE).getValue(), "Expected AGGRO_RANGE attribute to have a value of 16.0 after modifier was added");
+		helper.assertValueEqual(16.0, attribute.getValue(), "Expected AGGRO_RANGE attribute to have a value of 16.0 after modifier was added");
 		helper.succeed();
 	}
 
