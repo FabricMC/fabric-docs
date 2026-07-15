1e2f86b99fbf06d7d3905d4b6ca2607bf089c457
Deximus-Maximus <kev12east@gmail.com>
2026-07-15 15:45:25 -0400

    Revert "Change formating on range computation link"
    This reverts commit f9e2b90b630b877d294dc51703da5886ef822b67.

--- .vitepress/theme/components/Range.vue
+++ .vitepress/theme/components/Range.vue
@@ -7,7 +7,9 @@ const props = defineProps<{ r: string }>();
 
 const data = useData();
 
-const href = computed(() => `https://dexman545.github.io/outlet-database/floaderValidator?mode=minecraft&p=${encodeURIComponent(props.r)}`);
+const href = computed(() => {
+  return `https://dexman545.github.io/outlet-database/floaderValidator.html?mode=minecraft&p=${encodeURIComponent(props.r)}`;
+});
 </script>
 
 <template>
