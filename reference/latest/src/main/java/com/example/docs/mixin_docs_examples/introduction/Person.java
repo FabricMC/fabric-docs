28f1c3663bec823d72cfbdefed69a20c68908261
GauntRecluse <introvertpale@gmail.com>
2026-07-28 21:43:57 +0200

    Run checkstyle

--- reference/latest/src/main/java/com/example/docs/mixin_docs_examples/introduction/Person.java
+++ reference/latest/src/main/java/com/example/docs/mixin_docs_examples/introduction/Person.java
@@ -1,30 +1,32 @@
 package com.example.docs.mixin_docs_examples.introduction;
+
 // Sanitized target class to act as a very intuitive and "simple" target for introductory docs.
 
 //#region mixin_introduction_person_example_target_class
 public class Person {
-    private final String name;
-    private int age;
+	private final String name;
+	private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public int getAge() {
-        return this.age;
-    }
+	public int getAge() {
+		return this.age;
+	}
 
-    protected void setAge(int newAge) {
-        this.age = newAge;
-    }
+	protected void setAge(int newAge) {
+		this.age = newAge;
+	}
 
-    public boolean canLegallyDrink() {
-        return this.age >= 21;
-    }
+	public boolean canLegallyDrink() {
+		return this.age >= 21;
+	}
 }
+
 //#endregion mixin_introduction_person_example_target_class
