bf4fe5d1623e8ce1311a98ef5af4224425771375
CelDaemon <celdaemon@voidgroup.net>
2026-08-13 20:58:27 +0200

    Make checkstyle content with broken dead code

--- reference/latest/src/main/java/com/example/docs/menu/custom/SuperiorUpgradingMenu.java
+++ reference/latest/src/main/java/com/example/docs/menu/custom/SuperiorUpgradingMenu.java
@@ -5,7 +5,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 
 @SuppressWarnings("unused")
 public class SuperiorUpgradingMenu extends UpgradingMenu {
