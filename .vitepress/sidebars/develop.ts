bd8370140036ec2de8bab0020413ef0bb908b1c3
Jimmy474 <98521564+Jimmy474@users.noreply.github.com>
2026-09-15 09:49:49 -0400

    Added dynamic registry documentation and example implementation (#582)
    Co-authored-by: +merlan #flirora <flirora@flirora.xyz>
    Co-authored-by: Miroma <its.miroma@proton.me>
    Co-authored-by: Cassian <cassian@godsted.com>
    Signed-off-by: jimmy <vashijaimin474@gmail.com>

--- .vitepress/sidebars/develop.ts
+++ .vitepress/sidebars/develop.ts
@@ -365,6 +365,16 @@ export default [
     ],
   },
   {
+    text: "develop.registries",
+    collapsed: true,
+    items: [
+      {
+        text: "develop.registries.dynamic_registries",
+        link: "/develop/registries/dynamic-registry",
+      },
+    ],
+  },
+  {
     text: "develop.serialization",
     collapsed: true,
     items: [
